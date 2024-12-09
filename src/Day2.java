6f7eb7066d21312b4ffcdb7f3d7c1a7ce7ac40ae
BT_4N2_34 <sample@bths.edu>
2024-12-09 13:21:09 -0500

    Day 2 Part 1 incomplete

--- src/Day2.java
+++ src/Day2.java
@@ -11,20 +11,49 @@ public class Day2 {
         for (int i = 0; i < list.size(); i++) {
             ArrayList<Integer> report = new ArrayList<>();
             String repString = list.get(i);
+            System.out.println(repString);
             while (repString.indexOf(" ") > 0) {
                 report.add(Integer.parseInt(repString.substring(0,repString.indexOf(" "))));
                 repString = repString.substring(repString.indexOf(" ")+1);
             }
             report.add(Integer.parseInt(repString));
+            System.out.println(report);
             boolean safe = true;
+            boolean increasing = report.get(0) - report.get(1) <= 0;
             for (int n = 0; n < report.size() - 1; n++) {
-                if (!((report.get(n) - report.get(n+1)) >= -3 && (report.get(n) - report.get(n+1)) <= 3)) {
+                if(increasing && report.get(n) - report.get(n+1) > 0) {
+                    System.out.println(report.get(n));
+                    System.out.println(report.get(n+1));
                     safe = false;
+                    System.out.println("increasing error");
+                    break;
+                }
+                if(report.get(n) - report.get(n+1) > 3) {
+                    System.out.println(report.get(n));
+                    System.out.println(report.get(n+1));
+                    safe = false;
+                    System.out.println("greater than 3");
+                    break;
+                }
+                if(report.get(n) - report.get(n+1) < -3) {
+                    System.out.println(report.get(n));
+                    System.out.println(report.get(n+1));
+                    safe = false;
+                    System.out.println("less than -3");
+                    break;
+                }
+                if (report.get(n) - report.get(n+1) == 0) {
+                    System.out.println(report.get(n));
+                    System.out.println(report.get(n+1));
+                    safe = false;
+                    System.out.println("zero error");
+                    break;
                 }
             }
             if (safe) {
                 count++;
             }
+            System.out.println("increasing: " + increasing +"\nSafe: " + safe);
         }
         System.out.println("2024 Advent Day 2 Part 1 -\nTotal safe reports: " + count);
     }
