7b2aace18c811724b392d42e0d677c36b6229ac4
BT_4N2_34 <sample@bths.edu>
2024-12-11 13:03:44 -0500

    Day 2 Part 1 solved

--- src/Day2.java
+++ src/Day2.java
@@ -18,42 +18,11 @@ public class Day2 {
             }
             report.add(Integer.parseInt(repString));
             System.out.println(report);
-            boolean safe = true;
-            boolean increasing = report.get(0) - report.get(1) <= 0;
-            for (int n = 0; n < report.size() - 1; n++) {
-                if(increasing && report.get(n) - report.get(n+1) > 0) {
-                    System.out.println(report.get(n));
-                    System.out.println(report.get(n+1));
-                    safe = false;
-                    System.out.println("increasing error");
-                    break;
-                }
-                if(report.get(n) - report.get(n+1) > 3) {
-                    System.out.println(report.get(n));
-                    System.out.println(report.get(n+1));
-                    safe = false;
-                    System.out.println("greater than 3");
-                    break;
-                }
-                if(report.get(n) - report.get(n+1) < -3) {
-                    System.out.println(report.get(n));
-                    System.out.println(report.get(n+1));
-                    safe = false;
-                    System.out.println("less than -3");
-                    break;
-                }
-                if (report.get(n) - report.get(n+1) == 0) {
-                    System.out.println(report.get(n));
-                    System.out.println(report.get(n+1));
-                    safe = false;
-                    System.out.println("zero error");
-                    break;
-                }
-            }
+            boolean safe = numDiff(report,increasing(report));
             if (safe) {
                 count++;
             }
-            System.out.println("increasing: " + increasing +"\nSafe: " + safe);
+            System.out.println("Safe: " + safe);
         }
         System.out.println("2024 Advent Day 2 Part 1 -\nTotal safe reports: " + count);
     }
@@ -72,5 +41,34 @@ public class Day2 {
         catch (FileNotFoundException e) {
             return fileData;
         }
+    }
+
+    public static boolean increasing(ArrayList<Integer> report) {
+        if (report.get(0) < report.get(1)) {
+            return true;
+        } else if (report.get(0) > report.get(1)) {
+            return false;
+        } else {
+            return false;
+        }
+    }
+
+    public static boolean numDiff(ArrayList<Integer> report, boolean isIncreasing) {
+        if (isIncreasing) {
+            for (int i = 0; i < report.size() - 1; i++) {
+                if (report.get(i) - report.get(i + 1) < -3
+                || report.get(i) - report.get(i + 1) > -1) {
+                    return false;
+                }
+            }
+        } else {
+            for (int i = 0; i < report.size() - 1; i++) {
+                if (report.get(i) - report.get(i + 1) > 3
+                || report.get(i) - report.get(i + 1) < 1) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 }
