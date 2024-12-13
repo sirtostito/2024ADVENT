41295b05f5817ae318b67f1c646426ef9c6ec88a
BT_4N2_34 <sample@bths.edu>
2024-12-13 13:20:41 -0500

    Completed Day 3 Pt 1

--- src/Day3.java
+++ src/Day3.java
@@ -7,9 +7,18 @@ public class Day3 {
     public static void main(String[] args) {
         ArrayList<String> instructions = getFileData("inputs/day3");
         int total = 0;
+        int total2 = 0;
+        boolean enabled = true;
         for (int lines = 0; lines < instructions.size(); lines++) {
             String instruction = instructions.get(lines);
             while (instruction.contains("mul(")) {
+                if (instruction.indexOf("do()") < instruction.indexOf("mul(")) {
+                    enabled = true;
+                }
+                if (instruction.indexOf("don't()") < instruction.indexOf("mul(")) {
+                    enabled = false;
+                }
+                total2 += multiply(instruction,enabled);
                 int mul = instruction.indexOf("mul("); // first index of mul
                 String mulString;
                 if (instruction.length() - 1 < mul+11) {
@@ -27,19 +36,15 @@ public class Day3 {
                             Checks if the first field delimited by comma is numerical
                             Checks if the second field delimited by comma is numerical
                              */
-                            System.out.println(mulString);
-                            System.out.println(Integer.parseInt(nums[0]) + " " + Integer.parseInt(nums[1]));
-                            System.out.println(Integer.parseInt(nums[0]) * Integer.parseInt(nums[1]));
-                            System.out.println("Total: " + total);
                             total += Integer.parseInt(nums[0]) * Integer.parseInt(nums[1]);
-                            System.out.println("New total: " + total);
                         }
                     }
                 }
                 instruction = instruction.substring(mul+1);
             }
         }
-        System.out.println(total);
+        System.out.println("Day 3 Pt 1 - " + total
+        + "\nDay 3 Pt 2 - " + total2);
     }
     public static ArrayList<String> getFileData(String fileName) {
         ArrayList<String> fileData = new ArrayList<String>();
@@ -56,5 +61,60 @@ public class Day3 {
         catch (FileNotFoundException e) {
             return fileData;
         }
+    }
+
+    public static int multiply(String string, boolean enabled) {
+        if (enabled) {
+            if (string.indexOf("mul(") < string.indexOf("don't()")) {
+                int mul = string.indexOf("mul("); // first index of mul
+                String mulString;
+                if (string.length() - 1 < mul + 11) {
+                    mulString = string.substring(mul, mul + 11); // highest possible index for a valid mulString
+                } else {
+                    mulString = string.substring(mul);
+                }
+                if (mulString.contains(")")) {
+                    mulString = mulString.substring(0, mulString.indexOf(")") + 1);
+                    if (mulString.contains(",")) {
+                        String[] nums = mulString.substring(4, mulString.length() - 1).split(",");
+                        if (nums.length == 2 && nums[0].chars().allMatch(Character::isDigit) && nums[1].chars().allMatch(Character::isDigit)) {
+                            /*
+                            Checks if there is only one comma
+                            Checks if the first field delimited by comma is numerical
+                            Checks if the second field delimited by comma is numerical
+                             */
+                            return Integer.parseInt(nums[0]) * Integer.parseInt(nums[1]);
+                        }
+                    }
+                }
+            }
+        }
+        return 0;
+    }
+    public static int multiply(String string) {
+        if (string.indexOf("mul(") < string.indexOf("don't()")) {
+            int mul = string.indexOf("mul("); // first index of mul
+            String mulString;
+            if (string.length() - 1 < mul + 11) {
+                mulString = string.substring(mul, mul + 11); // highest possible index for a valid mulString
+            } else {
+                mulString = string.substring(mul);
+            }
+            if (mulString.contains(")")) {
+                mulString = mulString.substring(0, mulString.indexOf(")") + 1);
+                if (mulString.contains(",")) {
+                    String[] nums = mulString.substring(4, mulString.length() - 1).split(",");
+                    if (nums.length == 2 && nums[0].chars().allMatch(Character::isDigit) && nums[1].chars().allMatch(Character::isDigit)) {
+                            /*
+                            Checks if there is only one comma
+                            Checks if the first field delimited by comma is numerical
+                            Checks if the second field delimited by comma is numerical
+                             */
+                        return Integer.parseInt(nums[0]) * Integer.parseInt(nums[1]);
+                    }
+                }
+            }
+        }
+        return 0;
     }
 }
